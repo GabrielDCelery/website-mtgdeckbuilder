2d1b956200ef616279cf0ebe377be6e97934d8ef
Gabriel D. Celery <gabriel.d.celery@gmail.com>
2016-02-14 12:41:00 +0100

    Added basic gulp settings and configuration for development

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('connect-livereload')());
 
 app.get('*', function (req, res){
 	res.sendFile(__dirname + '/public/main.html');
 })
 
 app.listen(3000, function(){
-	console.log('server is running at port 3000...');
+	console.log('server is running at port 3000....');
 })
